417607d29aaba4a5f3b12f39caf1a4b974f28843
Anton Kadantsev <kadantsev.anton@yandex.ru>
2015-12-08 09:50:49 +0300

    asf

---
+++ wp-content/themes/healthy-food-2/footer.php
@@ -0,0 +1,1 @@
+<div class="endline"></div><!-- footer start -->	<div id="footer" class="clearfix">		<div class="credit">Все права защищены  </div>		</div>	</div><!-- footer end --></div></div></div></div><!-- wrapper end --><?php wp_footer(); ?></body></html><?
---
+++ wp-content/themes/healthy-food-2/welcome.php
@@ -0,0 +1,3 @@
+<!-- replace the content with your welcome text -->
+Приветствую Вас на своём блоге! Я повор в 10-м поколении.
+
